3ff3e28aa47c5ee8bd6d4ff167edb317cb7ad0e9
kl4z-02 <114046397+kl4z-02@users.noreply.github.com>
2023-02-17 09:49:07 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "SOMETHING TO SHOW NGROK WOKRING";
   cout << "I have successfully built and run\n";
   return 0;
 }
